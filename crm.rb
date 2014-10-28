c167113bcc8f385763d87c7b460e11650f7d5fce
Manny Berkal-Sarbit <mannyberkalsarbit@gmail.com>
2014-10-28 14:43:03 -0400

    lots of new stuff

---
+++ crm.rb
@@ -0,0 +1,19 @@
+require_relative 'contact'
+require_relative 'rolodex'
+require 'sinatra'
+
+$rolodex= Rolodex.new 
+
+#routes
+get '/' do 
+	@crm_app_name = "My CRM"
+	erb :index
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+get '/contacts/new' do
+	erb :new_contact
+end
